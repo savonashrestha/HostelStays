bf6709515ae404c3016a9e032ae4d2627e54dac5
Savona Shrestha <np03cs4a220177@heraldcollege.edu.np>
2024-04-14 14:36:09 +0545

    Added Meals Page

--- src/pages/meals/Meals.jsx
+++ src/pages/meals/Meals.jsx
@@ -7,9 +7,47 @@ function Meals() {
   return (
     <div className='meals'>
       <Sidebar/>
-      <div className="mealsContent">
+      <div className="mealscontext">
         <Navbar/>
-        <h1>Meals</h1>
+        <div className='meals-container'>
+          <h2>Add / Update Meals</h2>
+          <div className='days-list'>
+            <label>
+              Day: 
+            <select name="days" className='dayname'>
+              <option value="Sunday">Sunday</option>
+              <option value="Monday">Monday</option>
+              <option value="Tuesday">Tuesday</option>
+              <option value="Wednesday">Wednesday</option>
+              <option value="Thursday">Thursday</option>
+              <option value="Firday">Friday</option>
+              <option value="Saturday">Saturday</option>
+            </select>
+          </label>
+        </div>
+        <h4>Vegeterian</h4>
+        <div className='box'>
+          <input type="text" placeholder='Breakfast' autoComplete='Off'/>
+        </div>
+        <div className='box'>
+          <input type="text" placeholder='Lunch' autoComplete='Off'/>
+        </div>
+        <div className='box'>
+          <input type="text" placeholder='Dinner' autoComplete='Off'/>
+        </div>
+        <h4>Non - Vegeterian</h4>
+        <div className='box'>
+          <input type="text" placeholder='Breakfast' autoComplete='Off'/>
+        </div>
+        <div className='box'>
+          <input type="text" placeholder='Lunch' autoComplete='Off'/>
+        </div>
+        <div className='box'>
+          <input type="text" placeholder='Dinner' autoComplete='Off'/>
+        </div>
+        <button>Add</button>
+        <button>Update</button>
+      </div>
       </div>
     </div>
   )
